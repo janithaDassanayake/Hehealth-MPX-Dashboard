47dfe3ba59f24c8916fed466294ce824fecc2b9f
dasunsennath <dasunsennath123@gmail.com>
2022-08-02 16:26:06 +0530

    First Commit

---
+++ handle.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){ 
+    $(window).scroll(function(){ 
+        if ($(this).scrollTop() > 100) { 
+            $('#scroll').fadeIn(); 
+        } else { 
+            $('#scroll').fadeOut(); 
+        } 
+    }); 
+    $('#scroll').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 600); 
+        return false; 
+    }); 
+});
+
+
+if (window.navigator.userAgent.indexOf("Mobile") > -1) {
+    // HIDING ELEMENTS
+    $(".scroll").hide();
+}    
